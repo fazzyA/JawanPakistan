1d6b4340c39d7443ba9cc4fc86613c5713924b94
fazzyA <faz.pak@gmail.com>
2021-09-12 20:50:24 +0500

    a2

---
+++ Assignments/assignment2.js
@@ -0,0 +1,10 @@
+// write a function which returns array. call the function and destructure array values and print the value
+
+// Make a car object with model, year, color. display all the values by destructuring
+
+// use three variable name, greeting and display value using template literals
+
+// use array [1,2,3,4,5,6,7,8,9,10] assign first three values in seperate variables and all remaining in one element as an array
+
+// if car speed is less than 80 display "car speed is slow", if greater than 80 but less than 120 than display 
+// " car speed is moderate" and if greater than 120 display "car speed is fast"
---
+++ advance-javascript-slides-code/class2.js
@@ -0,0 +1,48 @@
+//var hoisting, template literals, destructuring array n objects
+//VARIABLE HOISTING
+// Try any part one at a time
+
+// var myname;         // Declare Variable
+// myname = 'Aamna';   // Initialize variable
+// console.log(myname) // result: Aamna
+
+
+
+/*********************************** */
+
+// var myname;          // Declare Variable
+// console.log(myname)  // result: Undefined
+// myname = 'Aamna';    // Initialize variable
+
+
+
+/*********************************** */
+
+// console.log(myname)  // 
+// myname = 'Aamna';    // 
+// var myname;          // 
+
+
+/*********************************** */
+
+// console.log(myname)  
+//myname = 'Aamna';     
+
+/*********************************** */
+// var a = 'Tom n Jerry'; // Global scope
+// function iota() {
+//     var a = 'Momma'; // 
+//     console.log(a); // 
+//     console.log(window.a);
+// }
+// iota();
+// console.log(a);
+////////Template literal//////
+
+// Display your name and module name in seperate lines using alert and single quotes
+
+///destructuring
+ const arr = ["Yusra","Nashra","Faiza"]
+
+ let firstST = arr[0]
+ let secST = arr[1]
