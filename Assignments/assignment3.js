64b819b0d6d3fc1b9d8f4240fb2a3a22831704a1
fazzyA <faz.pak@gmail.com>
2021-09-20 18:06:39 +0500

    asgmnt3

---
+++ Assignments/assignment3.js
@@ -0,0 +1,12 @@
+//write a program which shows the couter after each second.
+
+// write a function which takes your name and displays the greeting with your name
+
+// write a function(arrow function) which takes two values and return its sum as a result
+
+//write a function(arrow function) which takes a number and multiply it with 0.5 and return the new value. print the new value outside the function
+
+//print simple array of [1,2,3,4,5] with the help of array map funtion
+
+// let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]
+// iterate the given array through map function and print the name and id
