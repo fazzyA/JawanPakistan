88bd223a96f44345b90949e1104544a27e541af9
fazzyA <faz.pak@gmail.com>
2021-09-25 18:31:56 +0500

    update

---
+++ advance-javascript-slides-code/class4.js
@@ -0,0 +1,16 @@
+//array functions(forEach,map, find, filter, some,reduce ) promises
+//print array with foreach
+//convert lower case names array in sentence case
+//convert number array into new array
+//find students below 18 age
+// given num array. extract even numbers in other array
+const myStudent = [
+    {id:1, name:"aaa", age: 15},
+    {id:2, name:"bbb", age: 16},
+    {id:3, name:"ccc", age: 18},
+    {id:4, name:"ddd", age: 22},
+    {id:4, name:"zzz", age: 12},
+]
+
+
+
---
+++ advance-javascript-slides-code/myCode.js
@@ -0,0 +1,30 @@
+// let myPromise = new Promise((resolve,reject)=>{
+//     // resolve('yeeee')
+// setTimeout(()=>resolve('yyyyy'),5000)
+// })
+
+
+// myPromise.then(res=>console.log(res)).catch(err=>console.log(err))
+
+
+
+
+// let youngStudents  = myStudent.filter(item=>item.age<18)
+
+// youngStudents.forEach(item=>console.log(item))
+
+// let toRemove  = myStudent.find(item=>item.age=12)
+// console.log(toRemove);
+
+// const num = [1,2,3]
+// console.log(num.reduce((a,b)=>a+b,0))
+// let double = num.map((item,ind)=>{
+//     return item*2
+// })
+
+// const str = ['faiza','mughal','hafsa']
+
+// let capitalize = str.map((item)=>{
+//     return item[0].toUpperCase()+item.slice(1)
+// })
+// console.log(capitalize);
