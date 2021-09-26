83ddf9827e6c46818f8bd2f3aa924e66b19f284d
fazzyA <faz.pak@gmail.com>
2021-09-26 19:00:55 +0500

    a4

---
+++ Assignments/assignment4.js
@@ -0,0 +1,57 @@
+//We have an array of cities. Assign each city in different individual variable. hint ['LA', 'new york', 'karachi','islamabad']
+
+//We have a student object. Assign each property in different and individual variable in JS {name:'Marry',age:20,rollNo:2}
+
+//A function is taking 5 arguments and You have to pass it through an array.
+//let arr = [1,2,3,4,5]
+
+//Given an array of firstname and an array of last name. First check both array size is equal or not, if equal then make third new array which has first last name concat value
+//let firstNames = ['maryam', 'hira', 'faiza', 'aisha']
+//let lastNames = ['khan', 'ayub', 'aziz', 'yasin']
+//['maryam khan', 'hira ayub', 'faiza aziz', 'aisha yasin']
+
+//Create an arrow function that takes array as an input and make each element double and then return an array 
+
+//Create an arrow function that takes a number and return square of a number
+
+/*question: `var authenticated=true;
+var allowed = authenticated ? (alert('you r allowed')) : (alert('you are not allowed))
+which alert will run
+*/
+
+/*
+question: ` What is the value of canDrive
+        var age = 12;
+        var canDrive = age > 18 ? 'yes' : 'no';
+        `,
+        answers: ['12', '18', 'yes', 'no'],
+        which is correct answer
+        */
+
+/*
+question: `const dog='snickers'
+        logdog(dog);
+
+        function logdog(dog){
+            console.log(dog)
+        }
+         function go(){
+             const dog='sunny';
+             logdog(dog)
+         }`,
+        answers: ['snickers sunny','sunny snickers','snickers snickers','sunny sunny'] */
+
+
+//let text = `He's often called "Johnny"`;
+// console.log(text)// write the answer here
+
+ 
+/*function welcomeMsg(name = "Everyone") {
+    let msg = "Welcome " + name
+    console.log(msg)
+}
+
+// welcomeMsg("Faiza")
+// welcomeMsg()
+I have called funtion twice , share the answer what is printed each time
+*/
